b26da5473b4c7a65a58631bca2827ffee2a126da
yzc92 <91071667+yzc92@users.noreply.github.com>
2021-09-20 22:30:18 +0800

    Update Test.java

--- src/main/java/com/xixi/test/Test.java
+++ src/main/java/com/xixi/test/Test.java
@@ -4,5 +4,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("ncsdncs");
         System.out.println("提交成功");
+        System.out.println("修改了");
     }
 }
